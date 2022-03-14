ea6d0622dc1bb27df2520f878641662ca77555d6
Stefan Lehmann <bernd49634@web.de>
2022-03-14 02:32:56 +0100

    adjusted integration VRF test

--- test/integration/VRFConsumer.js
+++ test/integration/VRFConsumer.js
@@ -1,11 +1,13 @@
-const { expect } = require("chai");
+const { expect, should } = require("chai");
 const { ethers } = require("hardhat");
 const { utils } = require("ethers");
+
 const LinkTokenABI =
   '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transferAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"data","type":"bytes"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]';
 describe("Choose Winner Integration, VRF", () => {
   let lt;
   beforeEach(async () => {
+    // deploying the lottery contract
     const LT = await ethers.getContractFactory("Lottery");
     lt = await LT.deploy(
       "0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9",
@@ -16,62 +18,66 @@ describe("Choose Winner Integration, VRF", () => {
     await lt.deployed();
   });
   it("should choose a winner after entering the lottery 3 times (all different addresses)", async () => {
-    // getting 3 different addresses
+    // getting signer for account address
     const accounts = await ethers.getSigners();
     const signer = accounts[0];
+    // creating a new instance of the linkTokenContract to use his functions
     const linkTokenContract = new ethers.Contract(
       "0xa36085F69e2889c224210F603D836748e7dC0088",
       LinkTokenABI,
       signer
     );
+    // transfering 2 link from tester account address to the newly deployed lottery testing contract
     var transferTransaction = await linkTokenContract.transfer(
       lt.address,
       "2000000000000000000"
     );
+    // waiting for the transaction to fulfill
     await transferTransaction.wait();
     console.log("hash:" + transferTransaction.hash);
+    // entering the pool 2 times with owner address (doesn't matter which address we use)
     await lt.enterPool({
       value: utils.parseUnits("0.002", "ether"),
     });
     await lt.enterPool({
       value: utils.parseUnits("0.002", "ether"),
     });
+    // waiting for the lottery time interval to end (10 seconds), so we can keep testing the newly available functions
     await new Promise((resolve) => setTimeout(resolve, 11000));
-    let result = await expect(lt.chooseWinner());
-    console.log(result);
+    // choosing the owner of the lottery -> calling getRandomNumber VRF
+    await expect(lt.chooseWinner());
+    // it takes some time until the results of the VRF are in. We'll wait 60 seconds for the result to come in
     await new Promise((resolve) => setTimeout(resolve, 60000));
-    result = await lt.randomResult();
-    console.log(result);
-    /* const [owner, addr1, addr2] = await ethers.getSigners();
-    // entering the pool 3 times with individual addresses
+    // we are checking if the result is between the possible indexes (0-1 in this case)
+    expect(utils.formatUnits(await lt.randomResult(), 0)).to.satisfy(function (
+      num
+    ) {
+      if (num === "0" || num === "1") {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    /*  .to.be.greaterThan(-1)
+      .and.smallerThan("2"); */
+
+    // changing entry price
+    await lt.entryPriceInWei(utils.parseUnits("0.003", "ether"));
+    // checking if entry price changed successfully
+    expect(utils.formatEther(await lt.price())).to.equal("0.003");
+    // changing the interval which the lottery is atleast running for
+    await lt.settingTimeInSeconds(20);
+    // checking if the interval changed to 20 seconds
+    expect(await lt.time()).to.equal("20");
+    // we shouldn't be able to call the chooseWinner function again without starting a new lottery
+    await expect(lt.chooseWinner()).to.be.revertedWith(
+      "You need to choose the winner for the current Lottery, before you can start a new one"
+    );
+    // starting a new lottery
+    await lt.startNewLottery();
+    // entering the pool once
     await lt.enterPool({
       value: utils.parseUnits("0.002", "ether"),
     });
-    await expect(lt.chooseWinner()).to.be.revertedWith(
-      "The lottery has not ended yet"
-    );
-    await lt.connect(addr1).enterPool({
-      value: utils.parseUnits("0.002", "ether"),
-    });
-    await lt.connect(addr2).enterPool({
-      value: utils.parseUnits("0.002", "ether"),
-    });
-    // checking that the array includes all new entries
-    expect(await lt.getArrayLength()).to.equal("3");
-    // checking each individual index to check if different addresses are picked up and saved correctly
-    expect(await lt.participants(0)).to.equal(owner.address);
-    expect(await lt.participants(1)).to.equal(addr1.address);
-    expect(await lt.participants(2)).to.equal(addr2.address);
-
-    await expect(lt.chooseWinner()).to.be.revertedWith(
-      "The lottery has not ended yet"
-    );
-    // waiting for the time interval to run out
-    await ethers.provider.send("evm_increaseTime", [10]);
-    let result = await expect(lt.chooseWinner());
-    console.log(result);
-    await new Promise((resolve) => setTimeout(resolve, 60000)); */
-    /* result = await lt.randomResult();
-    console.log(result); */
   });
 });
